52fad183a5c8eaac6c1da38e8eb44f668cc34555
Hoàng Bảo <lehoangbao27092000hiie@gmail.com>
2022-01-10 10:11:18 +0700

    first

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import homepage from "./screens/Homepage/Home"
+import Register from "./screens/Register/register"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+export default createAppContainer(
+  createStackNavigator(
+    { Register, homepage, },
+    { initialRouteName: "homepage" }
+  )
+);
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
---
+++ screens/Homepage/Home.js
@@ -0,0 +1,39 @@
+import React, { createContext } from "react";
+import { Pressable, StyleSheet, TextInput, View, Text } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import styles from "./Styles";
+
+const Home = ({navigation}) => {
+  const [studentId, setStudentId] = React.useState(null);
+  const [classId, setClassId] = React.useState(null);
+  const [email, setEmail] = React.useState(null);
+  const [name, setName] = React.useState(null);
+  const [password, setPassword] = React.useState(null);
+  const [buttonPresses, setButtonPresses] = React.useState(null);
+
+  React.useEffect(() => {
+    console.log(studentId);
+  }, [studentId]);
+
+  return (
+    <View style={styles.container}>
+      {/* <LinearGradient
+          colors={["#5ca0f2", "#f5f7f6"]}
+          start={[0.1, 0.1]}
+          style={styles.body}
+        >
+          <Text style={styles.text} >VCL</Text>
+      </LinearGradient> */}
+        
+      <Pressable style={styles.button} onPress={() => navigation.navigate("Register")}>
+      <Text>Register</Text>
+      </Pressable>
+      <Pressable style={styles.button} onPress={() => navigation.navigate("Search")}>
+      <Text>Search</Text>
+      </Pressable>
+
+    </View>
+  );
+};
+
+export default Home;
